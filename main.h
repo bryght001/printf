32c18af0844fcb094ba844446d693a95299223ca
bryght001 <brightchukwunwike@gmail.com>
2024-01-31 03:31:21 +0100

    $1

--- main.h
+++ main.h
@@ -5,54 +5,62 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...) {
-    int char_count = 0;
-    va_list args;
+int _printf(const char *format, ...)
+{
+int char_count = 0;
+va_list args;
 
-    va_start(args, format);
+va_start(args, format);
 
-    for (int i = 0; format[i] != '\0'; i++) {
-        if (format[i] != '%') {
-            putchar(format[i]);
-            char_count++;
-        } else {
-            i++;  // Move to the next character after '%'
-            switch (format[i]) {
-                case 'c': {
-                    char c = va_arg(args, int);  // Promote char to int for va_arg
-                    putchar(c);
-                    char_count++;
-                    break;
-                }
-                case 's': {
-                    char *str = va_arg(args, char *);
-                    while (*str) {
-                        putchar(*str);
-                        str++;
-                        char_count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    putchar('%');
-                    char_count++;
-                    break;
-                }
-                default: {
-                    // Handle invalid format specifier
-                    fprintf(stderr, "Invalid format specifier: %c\n", format[i]);
-                    char_count = -1;  // Indicate error
-                    goto cleanup;
-                }
-            }
-        }
-    }
-
-cleanup:
-    va_end(args);
-    return char_count;
+for (int i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+putchar(format[i]);
+char_count++;
+}
+else
+{
+i++;  /* Move to the next character after '%*/
+switch (format[i])
+{
+case 'c':
+{
+char c = va_arg(args, int);   /*Promote char to int for va_arg*/
+putchar(c);
+char_count++;
+break;
+}
+case 's':
+{
+char *str = va_arg(args, char *);
+while (*str)
+{
+putchar(*str);
+str++;
+char_count++;
+}
+break;
+}
+case '%': {
+putchar('%');
+char_count++;
+break;
+}
+default:
+{
+/*Handle invalid format specifier*/
+fprintf(stderr, "Invalid format specifier: %c\n", format[i]);
+char_count = -1;  /* Indicate error*/
+goto cleanup;
+}
+}
+}
 }
 
-
+cleanup:
+va_end(args);
+return (char_count);
+}
 
 #endif /* end of function */
