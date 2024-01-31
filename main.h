f53b5114793548cd465bce4231db2acd1dedaef3
bryght001 <brightchukwunwike@gmail.com>
2024-01-31 03:05:54 +0100

    $1

--- main.h
+++ main.h
@@ -1,15 +1,58 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
 #include <stdio.h>
-#include <stdarg.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdarg.h>
 
-int print_str(char *str);
-int _printf(const char *format, ...);
-int print_char(int c);
-int print_digit(long n, int base);
-int print_format(char specifier, va_list ap);
+int _printf(const char *format, ...) {
+    int char_count = 0;
+    va_list args;
 
-#endif
+    va_start(args, format);
+
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (format[i] != '%') {
+            putchar(format[i]);
+            char_count++;
+        } else {
+            i++;  // Move to the next character after '%'
+            switch (format[i]) {
+                case 'c': {
+                    char c = va_arg(args, int);  // Promote char to int for va_arg
+                    putchar(c);
+                    char_count++;
+                    break;
+                }
+                case 's': {
+                    char *str = va_arg(args, char *);
+                    while (*str) {
+                        putchar(*str);
+                        str++;
+                        char_count++;
+                    }
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    char_count++;
+                    break;
+                }
+                default: {
+                    // Handle invalid format specifier
+                    fprintf(stderr, "Invalid format specifier: %c\n", format[i]);
+                    char_count = -1;  // Indicate error
+                    goto cleanup;
+                }
+            }
+        }
+    }
+
+cleanup:
+    va_end(args);
+    return char_count;
+}
+
+
+
+#endif /* end of function */
