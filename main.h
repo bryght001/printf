0753a32985baf153537ff172d593879eed22e5f5
bryght001 <brightchukwunwike@gmail.com>
2024-02-01 23:30:23 +0100

    $1

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+int _printf(const char *format_string, ....){
+  va_list args;
+  va_start(
+    
+    )
+}
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
